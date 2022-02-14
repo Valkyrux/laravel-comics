14fc0d562dd21fcc88f1d18bf2362bbcd26d9a3f
Valkyrux <philadephos.granata@gmail.com>
2022-02-14 12:36:32 +0100

    init

---
+++ resources/views/guests/partials/footer.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <a href="#">dc power visa</a>
+            <a href="#">additional dc sites</a>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div>
+            <img src="" alt="">
+        </div>
+        <ul>
+            <li><a href="">characters</a></li>
+            <li><a href="">comics</a></li>
+            <li><a href="">movies</a></li>
+            <li><a href="">tv</a></li>
+            <li><a href="">games</a></li>
+            <li><a href="">collectibles</a></li>
+            <li><a href="">video</a></li>
+            <li><a href="">fans</a></li>
+            <li><a href="">news</a></li>
+            <li><a href="">shop</a></li>
+        </ul>
+        <form action="">
+            <input type="text" placeholder="Search">
+        </form>
+    </div>
+</header>
